37605be69a653c26da1b51e5cc2511b5b8556ce2
joaobatu10 <cadorbatujoaovitor@gmail.com>
2026-01-23 12:03:37 -0300

    fix axios baseURL for production

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  
     <BrowserRouter>
       <CssBaseline /> {/* Garante que o Material UI tenha estilos resetados */}
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  
 );
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // URL do backend FastAPI
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Interceptor para adicionar token automaticamente
