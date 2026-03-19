ed0235f3ba349c7563609b3fffb99cd653576833
joaobatu10 <cadorbatujoaovitor@gmail.com>
2026-03-19 14:46:03 -0300

    atualização

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const RAW_API_URL = import.meta.env.VITE_API_URL;
-export const API_URL = (RAW_API_URL || "http://127.0.0.1:8000").replace(/\/+$/, "");
+export const API_URL = (RAW_API_URL || "").replace(/\/+$/, "");
 
 console.log("API_URL em uso:", API_URL);
 
